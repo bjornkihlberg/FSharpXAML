9c7ee8a3b032646443876cd0a47e57678347c4c3
hoppfull <hoppfull@gmail.com>
2018-04-23 05:01:58 +0800

    uploaded fsxaml example

---
+++ App/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open System.Windows
+
+open FsXaml
+
+type MyWindow = XAML<"MyWindow.xaml">
+
+let MyButton_onClick _ = printfn "click!"
+
+[<STAThread; EntryPoint>]
+let main _ =
+    let myWindow = MyWindow()
+    myWindow.MyButton.Click.Add MyButton_onClick
+
+    Application().Run myWindow |> ignore
+    0 // return an integer exit code
